84eae0b381b2cb8fba4b1c6828b5f0dd3f1a858e
mindarelus <dimitar.nenov.dimitrov@abv.bg>
2021-02-22 17:02:36 +0200

    test increase version

--- src/events-manager.ts
+++ src/events-manager.ts
@@ -1,55 +1,53 @@
 class EventListener {
-    callback: Function;
-    context: any;
-    type: string;
+  callback: Function;
+  context: any;
+  type: string;
 
-    constructor(type: string, callback: Function, context: any) {
-        this.type = type;
-        this.callback = callback;
-        this.context = context;
-    }
+  constructor(type: string, callback: Function, context: any) {
+    this.type = type;
+    this.callback = callback;
+    this.context = context;
+  }
 }
 
+// Main class
 export class EventsManager {
+  constructor() {
+    this._listeners = [];
+  }
 
-    constructor() {
-        this._listeners = [];
+  private _listeners: EventListener[];
+
+  addEventListener(type: string, context: any, callback: Function) {
+    this._listeners.push(new EventListener(type, callback, context));
+  }
+
+  removeEventListener(type: string, listener: any) {
+    let i = 0;
+    const l: number = this._listeners.length;
+    let el: EventListener = null;
+    let index = -1;
+    for (i = 0; i < l; i++) {
+      el = this._listeners[i];
+      if (el.type === type && el.context === listener) {
+        index = i;
+        break;
+      }
     }
-
-    private _listeners: EventListener[];
-
-    addEventListener(type: string, context: any, callback: Function) {
-        this._listeners.push(new EventListener(type, callback, context));
+    if (index !== -1) {
+      this._listeners.splice(index, 1);
     }
+  }
 
-    removeEventListener(type: string, listener: any) {
-        let i = 0;
-        const l: number = this._listeners.length;
-        let el: EventListener = null;
-        let index = -1;
-        for (i = 0; i < l; i++) {
-            el = this._listeners[i];
-            if (el.type === type && el.context === listener) {
-                index = i;
-                break;
-            }
-        }
-        if (index !== -1) {
-            this._listeners.splice(index, 1);
-        }
+  dispatchEvent(type: string, data: any) {
+    let i = 0;
+    const l = this._listeners.length;
+    let listener: EventListener = null;
+    for (i = 0; i < l; i++) {
+      listener = this._listeners[i];
+      if (listener.type === type) {
+        listener.callback.call(listener.context, data);
+      }
     }
-
-    dispatchEvent(type: string, data: any) {
-        let i = 0;
-        const l = this._listeners.length;
-        let listener: EventListener = null;
-        for (i = 0; i < l; i++) {
-            listener = this._listeners[i];
-            if (listener.type === type) {
-                listener.callback.call(listener.context, data);
-            }
-        }
-    }
-
-
+  }
 }
